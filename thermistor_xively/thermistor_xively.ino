170b60103b2e3c576f182609b05a72e2338d4d3e
dakunier <github@dakunier.de>
2014-08-23 11:13:58 +0200

    thermistor scripts
    serialmonitor and xively upload script

---
+++ thermistor_xively/thermistor_xively.ino
@@ -0,0 +1,76 @@
+/*
+ Thermistor Xively Upload all 5Minutes
+ https://xively.com/feeds/1457109366
+ https://wiki.dakunier.de
+ https://github.com/Dakunier/arduino
+ twitter: @dakunier_de
+ */
+ 
+#include <SPI.h>
+#include <Ethernet.h>
+#include <HttpClient.h>
+#include <Xively.h>
+#include <math.h> //Temperatur Berechnung
+
+// MAC address for your Ethernet shield
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// Your Xively key to let you upload data
+char xivelyKey[] = "APIKEY";
+
+// Analog pin which we're monitoring (0 and 1 are used by the Ethernet shield)
+int sensorPin = 2;
+
+// Define the strings for our datastream IDs
+char sensorId[] = "Temperatur";
+XivelyDatastream datastreams[] = {
+  XivelyDatastream(sensorId, strlen(sensorId), DATASTREAM_FLOAT),
+};
+// Finally, wrap the datastreams into a feed
+XivelyFeed feed(FEEDID, datastreams, 1 /* number of datastreams */);
+
+EthernetClient client;
+XivelyClient xivelyclient(client);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Serial.println("Starting single datastream upload to Xively...");
+  Serial.println();
+
+  while (Ethernet.begin(mac) != 1)
+  {
+    Serial.println("Error getting IP address via DHCP, trying again...");
+    delay(15000);
+  }
+}
+
+float Thermister(int RawADC) {
+  float Temp;
+  Temp = log(((10240000 / RawADC) - 10000));
+  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
+  Temp = Temp - 273.15; //Celsius
+  return Temp;
+}
+
+void loop() {
+
+  int val;
+  double temp;
+  val = analogRead(2); //Analog Pin 2
+
+  float sensorValue = Thermister(val);;
+  datastreams[0].setFloat(sensorValue);
+
+  Serial.print("Read sensor value ");
+  Serial.println(datastreams[0].getFloat());
+
+  Serial.println("Uploading it to Xively");
+  int ret = xivelyclient.put(feed, xivelyKey);
+  Serial.print("xivelyclient.put returned ");
+  Serial.println(ret);
+
+  Serial.println();
+  delay(300000);
+}
